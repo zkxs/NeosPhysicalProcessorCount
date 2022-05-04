276de14c358d6b0b58d4d6ab3202ff0829c60196
Michael Ripley <zkxs00@gmail.com>
2022-05-04 06:08:46 -0500

    Initial commit

---
+++ NeosPhysicalProcessorCount/PhysicalProcessorCount.cs
@@ -0,0 +1,74 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace PhysicalProcessorCount
+{
+    public class PhysicalProcessorCount : NeosMod
+    {
+        public override string Name => "PhysicalProcessorCount";
+        public override string Author => "runtime";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/zkxs/NeosPhysicalProcessorCount";
+
+        private static bool calculated = false;
+        private static int? physicalCores = null;
+
+        public override void OnEngineInit()
+        {
+
+            Harmony harmony = new Harmony("dev.zkxs.NeosPhysicalProcessorCount");
+            harmony.PatchAll();
+        }
+
+        private static int? PhysicalCores()
+        {
+            if (!calculated)
+            {
+                physicalCores = QueryPhysicalCores();
+                calculated = true;
+            }
+            return QueryPhysicalCores();
+        }
+
+        private static int? QueryPhysicalCores()
+        {
+            try
+            {
+                int coreCount = 0;
+                foreach (var item in new System.Management.ManagementObjectSearcher("Select * from Win32_Processor").Get())
+                {
+                    coreCount += int.Parse(item["NumberOfCores"].ToString());
+                }
+                return coreCount;
+            }
+            catch (Exception e)
+            {
+                Error($"Error querying physical core count:\n{e}");
+                return null;
+            }
+        }
+
+        [HarmonyPatch]
+        private static class HarmonyPatches
+        {
+            [HarmonyPatch(typeof(StandaloneSystemInfo), nameof(StandaloneSystemInfo.PhysicalCores), MethodType.Getter)]
+            public static void PhysicalCoresPosfix(ref int? __result)
+            {
+                if (__result == null)
+                {
+                    __result = PhysicalCores();
+                    Debug($"");
+                }
+                else
+                {
+                    Warn("");
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# NeosPhysicalProcessorCount
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod that fixes Neos's physical processor count detection. This allows neos to use the intended number of worker threads without you having to manually use the `-BackgroundWorkers <n>` and `-PriorityWorkers <n>`  command line arguments.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [PhysicalProcessorCount.dll](https://github.com/zkxs/NeosPhysicalProcessorCount/releases/latest/download/PhysicalProcessorCount.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
